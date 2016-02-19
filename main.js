b52a3283d4232ad2e8ee250070a99b9a5039c77e
Emperorlou <nikolasarmstrong@gmail.com>
2016-02-19 09:08:17 -0500

    Setting up still...

--- main.js
+++ main.js
@@ -12,13 +12,14 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600, fullscreen:true, title:"Initium"});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL("https://www.playinitium.com/main.jsp");
+//  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+//  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {

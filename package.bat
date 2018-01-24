e5b211582f523e143aa8d348aab282fa071dcc9f
Emperorlou <nikolasarmstrong@gmail.com>
2018-01-24 13:55:17 -0500

    meh, prolly didn't work

--- package.bat
+++ package.bat
@@ -1,2 +1,2 @@
 cd ..\Initium-Electron-Builds
-C:\Electron-Packager-9.1.0\node_modules\.bin\electron-packager ..\Initium-Electron --all
+C:\Electron-Packager-9.1.0\node_modules\.bin\electron-packager ..\Initium-Electron --all --icon=..\Initium-Electron\app\phonegap\Initium-Phonegap\www\icon.ico
